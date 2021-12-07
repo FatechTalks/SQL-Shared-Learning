9874b9a1311ad9959769ce0accbb689fa9ed2658
RogerioXTI <sp.rogerio@outlook.com>
2021-12-07 20:11:05 -0300

    SQL Statement - INSERT INTO

---
+++ examples/insert_into_books.sql
@@ -0,0 +1,6 @@
+INSERT INTO books (name, pages, author, category, price) VALUES 
+('The Art of War', 50, 'Sun Tzu', 'Strategy', 100), 
+('Thus Spoke Zarathustra', 75, 'Friedrich Wilheim Nietzsche', 'Philosophy', 80), 
+('Tao the Ching', 66, 'Lao Tzu', 'Philosophy', 90), 
+('The Divine Comedy', 100, 'Dante Alighieri', 'Literature', 150), 
+('Sapiens', 120, 'Yuval Harari', 'Anthropology', 130);  
